c7de45fc6433b3713c9c9d489216f5175c96bc11
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-12-08 20:01:48 +0000

    New products

--- client/src/data/products.js
+++ client/src/data/products.js
@@ -1,105 +1,190 @@
-export const products = [
+export const featuredProducts = [
   {
-    image: './images/products/shirts/puma shirt.webp',
+    image: './images/products/shoes/puma-ignite-flash-evoknit.webp',
     rating: 4,
-    title: 'Pume - Essential Big Cat Tee - Grey Heather - Mens',
-    price: 30.99
-  },
-  {
-    image: './images/products/shirts/puma shirt.webp',
-    rating: 4,
-    title: 'Pume - Essential Big Cat Tee - Grey Heather - Mens',
-    price: 30.99
-  },
-  {
-    image: './images/products/shoes/running-shoes-2.webp',
-    rating: 5,
-    title: "Pume - Essential Big Cat Tee - Grey Heather - Women's",
+    title: 'Puma - Ignite Flash EvoKNIT - Black - Mens',
     price: 99.99
   },
   {
-    image: './images/products/shoes/Puma Sneaker.webp',
-    title: 'Puma - Ignite Flash EvoKNIT - Black - Mens',
-    rating: 4,
-    price: 30.99
-  },
-  {
-    image: './images/products/shoes/Puma Sneaker.webp',
-    title: 'Puma - Ignite Flash EvoKNIT - Black - Mens',
-    rating: 4,
-    price: 30.99
-  },
-  {
     image:
-      'https://www.decathlon.ie/media/catalog/product/cache/2/image/600x450/9df78eab33525d08d6e5fb8d27136e95/3/8/38_p1536138_8516951.jpg',
+      './images/products/shirts/puma-essential-big-cat-tree-grey-heather.webp',
+    rating: 3,
+    title: 'Pume - Essential Big Cat Tee - Grey Heather - Mens',
+    price: 30.99
+  },
+  {
+    image: './images/products/nike-pro-hypercool-capri-black-womens.webp',
     rating: 5,
     title: 'Nike - Pro Hypercool Capri - Black - Womens',
     price: 49.99
+  },
+  {
+    image:
+      './images/products/shoes/nike-free-rn-2017-running-shoe-black-mens.webp',
+    rating: 4,
+    title: 'Nike - Free RN 2017 Running Shoe - Black - Mens',
+    price: 99.99
+  },
+  {
+    image: './images/products/shoes/puma-ignite-flash-evoknit.webp',
+    rating: 4,
+    title: 'Puma - Ignite Flash EvoKNIT - Black - Mens',
+    price: 99.99
+  },
+  {
+    image:
+      './images/products/shirts/puma-essential-big-cat-tree-grey-heather.webp',
+    rating: 3,
+    title: 'Pume - Essential Big Cat Tee - Grey Heather - Mens',
+    price: 30.99
+  },
+  {
+    image: './images/products/nike-pro-hypercool-capri-black-womens.webp',
+    rating: 5,
+    title: 'Nike - Pro Hypercool Capri - Black - Womens',
+    price: 49.99
+  },
+  {
+    image:
+      './images/products/shoes/nike-free-rn-2017-running-shoe-black-mens.webp',
+    rating: 4,
+    title: 'Nike - Free RN 2017 Running Shoe - Black - Mens',
+    price: 99.99
   }
 ];
 
 export const highlightedProducts = [
   {
-    image: './images/products/shoes/Puma Sneaker.webp',
-    title: 'Puma - Ignite Flash EvoKNIT - Black - Mens',
-    rating: 4,
-    price: 30.99,
+    image: './images/products/shoes/adidas-originals-swift-run-cargo-mens.webp',
+    title: 'Adidas Originals - Swift Run - Black - Mens',
+    rating: 5,
+    price: 140.99,
     highlightPoints: [
       {
-        x: '50%',
-        y: '30%',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
-      },
-      {
-        x: '10%',
-        y: '70%',
-        text:
-          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis assumenda accusamus voluptates?'
-      },
-      {
-        x: '80%',
+        x: '30%',
         y: '20%',
         text:
           'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis assumenda accusamus voluptates?'
       },
       {
-        x: '50%',
-        y: '80%',
+        x: '44%',
+        y: '36%',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+      },
+      {
+        x: '14%',
+        y: '43%',
+        text:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis assumenda accusamus voluptates?'
+      },
+      {
+        x: '80%',
+        y: '50%',
+        text:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis assumenda accusamus voluptates?'
+      },
+      {
+        x: '60%',
+        y: '68%',
+        text:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis assumenda accusamus voluptates?'
+      },
+      {
+        x: '16%',
+        y: '74%',
         text:
           'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis assumenda accusamus voluptates?'
       }
     ]
   },
   {
-    image: './images/products/shoes/running-shoes-2.webp',
+    image:
+      "./images/products/shoes/floatride-fuel-run-women's-running-shoes.webp",
     rating: 5,
-    title: "Pume - Essential Big Cat Tee - Grey Heather - Women's",
+    title: "Reebok - Floatride Fuel Run - Blue - Women's",
     price: 99.99,
     highlightPoints: [
       {
         x: '10%',
-        y: '45%',
-        text:
-          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis assumenda accusamus voluptates?'
-      },
-      {
-        x: '40%',
         y: '35%',
         text:
           'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis assumenda accusamus voluptates?'
       },
       {
-        x: '80%',
-        y: '30%',
+        x: '10%',
+        y: '65%',
         text:
           'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis assumenda accusamus voluptates?'
       },
       {
-        x: '80%',
-        y: '60%',
+        x: '43%',
+        y: '32%',
+        text:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis assumenda accusamus voluptates?'
+      },
+      {
+        x: '55%',
+        y: '50%',
+        text:
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis assumenda accusamus voluptates?'
+      },
+      {
+        x: '90%',
+        y: '55%',
         text:
           'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis assumenda accusamus voluptates?'
       }
     ]
+  }
+];
+
+export const newProducts = [
+  {
+    image: './images/products/shoes/rebook-ex-o-fit_hi_black.webp',
+    rating: 4,
+    title: 'Reebook - Ex-O-Fit Hi - Black - Mens',
+    price: 179.99
+  },
+  {
+    image: './images/products/shoes/rebook-classic_nylon_white_standard.webp',
+    rating: 3,
+    title: 'Rebook - Classic Nylon - White/Pink - Womens',
+    price: 120.99
+  },
+  {
+    image: './images/products/shoes/nike-free-tr-v8.webp',
+    rating: 5,
+    title: 'Nike - Free TR V8 - Grey - Mens',
+    price: 109.99
+  },
+  {
+    image: './images/products/shoes/rebook-classic_nylon_shoes_black.webp',
+    rating: 4,
+    title: 'Rebook - Classic Slide - Black - Mens',
+    price: 200.99
+  },
+  {
+    image: './images/products/shoes/rebook-ex-o-fit_hi_black.webp',
+    rating: 4,
+    title: 'Reebook - Ex-O-Fit Hi - Black - Mens',
+    price: 179.99
+  },
+  {
+    image: './images/products/shoes/rebook-classic_nylon_white_standard.webp',
+    rating: 3,
+    title: 'Rebook - Classic Nylon - White/Pink - Womens',
+    price: 120.99
+  },
+  {
+    image: './images/products/shoes/nike-free-tr-v8.webp',
+    rating: 5,
+    title: 'Nike - Free TR V8 - Grey - Mens',
+    price: 109.99
+  },
+  {
+    image: './images/products/shoes/rebook-classic_nylon_shoes_black.webp',
+    rating: 4,
+    title: 'Rebook - Classic Slide - Black - Mens',
+    price: 200.99
   }
 ];
