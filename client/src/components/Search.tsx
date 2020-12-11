3ef847bfe09f3de400610ae8d759d3e2bca19aa6
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-12-11 02:30:01 +0000

    Navbar product search

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,14 +1,18 @@
-import * as React from "react";
-import styled from "styled-components";
-import theme from "../theme/theme";
-import { StyledInput } from "./styled";
-import { ReactComponent as SearchIcon } from "../assets/search.svg";
+import * as React from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
+import theme from '../theme/theme';
+import { StyledInput } from './styled';
+import { ReactComponent as SearchIcon } from '../assets/search.svg';
+import { allProducts } from '../data/products';
+import FormInput from './Form/FormInput';
 
 const StyledForm = styled.form`
   position: relative;
   width: 25rem;
   margin-left: auto;
   margin-right: 4rem;
+  font-size: 2rem;
 `;
 
 const StyledIcon = styled(SearchIcon)`
@@ -21,15 +25,69 @@ const StyledIcon = styled(SearchIcon)`
   height: 1.75rem;
 `;
 
+// TODO Refactor
+const ResultsContainer = styled.ul`
+  position: absolute;
+  background-color: white;
+  right: 0;
+  width: 150%;
+  z-index: 1;
+  border: 3px solid ${({ theme }) => theme.colors.primary};
+`;
+
+const StyledResult = styled.li`
+  padding: 1rem;
+  color: ${({ theme }) => theme.colors.gray};
+  display: flex;
+  align-items: center;
+  font-family: 'Bebas Neue';
+  transition: all 0.2s;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.lightGrey};
+  }
+`;
+
+const Results = ({ products }: { products: SimpleProduct[] }) => (
+  <ResultsContainer>
+    {products.map((product: SimpleProduct) => (
+      <Result product={product} />
+    ))}
+  </ResultsContainer>
+);
+
+const Result = ({ product }: { product: SimpleProduct }) => (
+  <StyledResult>
+    <div style={{ flex: '0 0 8rem', marginRight: '1rem' }}>
+      <img
+        style={{ width: '100%', objectFit: 'cover' }}
+        src={product.image}
+        alt={product.title}
+      />
+    </div>
+    <div>{product.title}</div>
+  </StyledResult>
+);
+
 const Search = () => {
+  const [query, setQuery] = useState('');
+  const [error, setError] = useState({ message: '' });
+
+  const filteredProducts = allProducts.filter(product =>
+    product.title.toLowerCase().includes(query.toLowerCase())
+  );
+
   return (
     <StyledForm>
       <StyledInput
-        color="white"
+        value={query}
+        onChange={e => setQuery(e.target.value)}
         placeholder="search"
+        color="white"
         backgroundColor={theme.colors.darkGrey}
       />
       <StyledIcon />
+      {query && <Results products={filteredProducts} />}
     </StyledForm>
   );
 };
