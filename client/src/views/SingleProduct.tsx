8e3d3c28aa41167016492a7dc50ea7cb8172db33
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-12-14 23:59:27 +0000

    Single product page

--- client/src/views/SingleProduct.tsx
+++ client/src/views/SingleProduct.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import { useState } from 'react';
+import styled from 'styled-components';
+import { allProducts } from '../data/products';
+import { RouteComponentProps, useParams, withRouter } from 'react-router-dom';
 
-const SingleProduct = () => {
-  return <div>s</div>;
+const Grid = styled.main`
+  display: grid;
+  grid-template-columns:
+    [full-start] minmax(6rem, 1fr) [center-start] repeat(
+      8,
+      [col-start] minmax(min-content, 14rem) [col-end]
+    )
+    [center-end] minmax(6rem, 1fr) [full-end];
+`;
+
+const ImageContainer = styled.div`
+  grid-column: center-start / span 3;
+`;
+
+const SingleProduct: React.FC = ({ match }: RouteComponentProps) => {
+  const { id } = useParams();
+
+  setProduct(
+    allProducts.find((product): product is Product => product._id === id)
+  );
+
+  return (
+    <Grid>
+      <ImageContainer>
+        <img src={product.image} alt={product.title} />
+      </ImageContainer>
+    </Grid>
+  );
 };
 
 export default SingleProduct;
