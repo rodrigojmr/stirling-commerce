def7895766a94bf5c013c80fac28dcd1aa14ed8b
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2020-12-16 19:20:48 +0000

    Decrease star margin

--- client/src/components/styled/Stars.tsx
+++ client/src/components/styled/Stars.tsx
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
 import { ReactComponent as Star } from '../../assets/star.svg';
 
+// TODO Refactor as Functional Component?
+
 export const StyledStar = styled(Star)<{ filled: boolean }>`
   stroke: none;
   width: 20px;
   height: 20px;
   fill: ${({ filled }) => (filled ? '#e69006' : '#a7a7a7')};
-  margin-right: 5px;
+  margin-right: 2px;
 `;
 
-export const Stars = (num: number) => {
+export const stars = (num: number) => {
   const starArray = [];
   for (let i = 1; i <= 5; i++) {
     starArray.push(<StyledStar filled={i <= num} />);
