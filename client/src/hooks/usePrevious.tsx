98ef7bdc49baf9d29ba993dadd75c5589a5b20c5
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2021-01-27 18:32:46 +0000

    usePrevious hook to use previous state

---
+++ client/src/hooks/usePrevious.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useRef } from 'react';
+
+const usePrevious = (value: any) => {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
+export default usePrevious;
