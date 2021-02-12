0e3209b42b46f55567d0573d3a439fe85115fb27
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2021-02-12 18:56:14 +0000

    Basic loader component

---
+++ client/src/components/Loader.tsx
@@ -0,0 +1,12 @@
+import { Center, Spinner } from '@chakra-ui/react';
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <Center>
+      <Spinner />
+    </Center>
+  );
+};
+
+export default Loader;
