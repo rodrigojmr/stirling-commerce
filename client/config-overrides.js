e93d346fa54a5058f7753a019c08d0d4a510fc98
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2021-02-21 21:48:04 +0000

    Update config overrides for paths

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,5 +1,13 @@
-const { alias, configPaths } = require('react-app-rewire-alias');
+const {
+  aliasDangerous,
+  configPaths,
+  CracoAliasPlugin
+} = require('react-app-rewire-alias/lib/aliasDangerous');
 
 module.exports = function override(config) {
-  return alias(configPaths('./tsconfig.paths.json'))(config);
+  aliasDangerous({
+    ...configPaths('tsconfig.paths.json')
+  })(config);
+
+  return config;
 };
