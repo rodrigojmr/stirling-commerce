bed4f686fa2cf93f87c72d62bc9e2a6b72d538be
Rodrigo Moura <rodrigojorgemoura@gmail.com>
2021-02-22 16:18:21 +0000

    Undo config override

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,19 +1,19 @@
-// const {
-//   aliasDangerous,
-//   configPaths,
-//   CracoAliasPlugin
-// } = require('react-app-rewire-alias/lib/aliasDangerous');
-
-// module.exports = function override(config) {
-//   aliasDangerous({
-//     ...configPaths('tsconfig.paths.json')
-//   })(config);
-
-//   return config;
-// };
-
-const { alias, configPaths } = require('react-app-rewire-alias');
+const {
+  aliasDangerous,
+  configPaths,
+  CracoAliasPlugin
+} = require('react-app-rewire-alias/lib/aliasDangerous');
 
 module.exports = function override(config) {
-  return alias(configPaths('./tsconfig.paths.json'))(config);
+  aliasDangerous({
+    ...configPaths('tsconfig.paths.json')
+  })(config);
+
+  return config;
 };
+
+// const { alias, configPaths } = require('react-app-rewire-alias');
+
+// module.exports = function override(config) {
+//   return alias(configPaths('./tsconfig.paths.json'))(config);
+// };
